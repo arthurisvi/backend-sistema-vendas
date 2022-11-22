a4c3e1acb3022100608c28cf1a60d9db1cbab800
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-21 21:25:25 -0300

    feat: customizing error handling

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -8,5 +9,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+
+} );
 
 app.listen(3333, () => console.log('Server is running!'));
