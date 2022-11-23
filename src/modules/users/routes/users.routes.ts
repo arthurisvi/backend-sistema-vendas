8e5036c0358c44bfeb84ad9049730ed18ffbe4e7
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-22 22:00:42 -0300

    feat: user routes

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate"
+import UsersController from "../controllers/UsersControllers";
+
+const usersRouter = Router();
+const usersController = new UsersController()
+
+usersRouter.get('/', usersController.index)
+
+usersRouter.post(
+  '/register',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required()
+    }
+  }),
+  usersController.store)
+
+
+export default usersRouter;
