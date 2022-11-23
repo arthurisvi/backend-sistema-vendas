4b29ea48590697d59501c798ca97e27655191cec
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-22 21:41:48 -0300

    feat: user repository

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,22 @@
+import { EntityRepository, Repository } from "typeorm";
+import User from "../entities/User";
+
+@EntityRepository(User)
+export default class UserRepository extends Repository<User> {
+
+  public async findByName(name: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: { name },
+    });
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: { email },
+    });
+
+    return user;
+  }
+}
