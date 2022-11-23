789b97ed242b452dae1dae32f303d6937b010de8
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-23 19:35:30 -0300

    feat: userToken entity

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Generated } from 'typeorm'
+
+@Entity('user_tokens')
+export default class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
