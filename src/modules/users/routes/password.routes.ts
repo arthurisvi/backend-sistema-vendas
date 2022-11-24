34a0a0e3608d31d300d26b00c4aa91e6e5e368d0
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-24 11:53:38 -0300

    feat: reset password

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { celebrate, Joi, Segments } from "celebrate"
 import ForgotPasswordController from "../controllers/ForgotPasswordController";
+import ResetPasswordController from "../controllers/ResetPasswordController";
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController()
+const resetPasswordController = new ResetPasswordController()
+
 
 passwordRouter.post(
   '/forgot',
@@ -13,6 +16,17 @@ passwordRouter.post(
     }
   }),
   forgotPasswordController.store)
+
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password'))
+    }
+  }),
+  resetPasswordController.store)
 
 
 export default passwordRouter;
