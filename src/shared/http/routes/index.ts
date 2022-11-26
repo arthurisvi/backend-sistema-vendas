cf9f71f33f8311a592d5d86aa337ec3431b46453
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-26 20:36:46 -0300

    feat: implementing order routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import customersRouter from '@modules/customers/routes/customers.routes';
+import ordersRouter from '@modules/orders/routes/order.routes';
 import authRouter from '@modules/users/routes/auth.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 
@@ -10,6 +11,7 @@ const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/customers', customersRouter);
+routes.use('/orders', ordersRouter);
 routes.use('/auth', authRouter);
 routes.use('/password', passwordRouter);
 
