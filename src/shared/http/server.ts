6e84bfe55e8e07ba1f108a8a06c1438a0bdeef8a
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-27 21:36:42 -0300

    feat: middleware limiting requests per second

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -7,11 +7,14 @@ import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import rateLimiter from "@shared/http/middlewares/rateLimiter";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(rateLimiter);
 
 app.use(routes);
 
