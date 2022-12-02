a9a460fb25e22d42504ce84df9de6b519313d853
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-12-02 01:24:25 -0300

    fix: adjusting save method in fake repository

--- src/modules/customers/services/tests/repositories/InMemoryCustomersRepository.ts
+++ src/modules/customers/services/tests/repositories/InMemoryCustomersRepository.ts
@@ -19,7 +19,9 @@ export default class InMemoryCustomersRepository implements ICustomersRepository
   }
 
   public async save(customer: Customer): Promise<Customer> {
-    Object.assign(this.customers, customer)
+    const findIndex = this.customers.findIndex(findCustomer => findCustomer.id === customer.id);
+    
+    this.customers[findIndex] = customer;
 
     return customer;
   }
