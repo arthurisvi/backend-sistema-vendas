2f943d9e6ab9c3ace6e98a3fb8c28c76b35350ea
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-12-02 01:20:17 -0300

    feat: creating test expected error

--- src/modules/customers/services/tests/CreateCustomerService.spec.ts
+++ src/modules/customers/services/tests/CreateCustomerService.spec.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import CreateCustomerService from "../CreateCustomerService";
 import InMemoryCustomersRepository from "./repositories/InMemoryCustomersRepository";
+import AppError from '@shared/errors/AppError';
 
 describe('CreateCustomer', () => {
   it('should be able to create a new customer', async () => {
@@ -13,5 +14,21 @@ describe('CreateCustomer', () => {
     })
 
     expect(customer).toHaveProperty('id');
+  })
+
+  it('should be able to create a new customer', async () => {
+    const customersRepository = new InMemoryCustomersRepository();
+    const createCustomerService = new CreateCustomerService(customersRepository);
+
+    await createCustomerService.execute({
+      name: 'Arthur Isvi',
+      email: 'spfcarthur1@gmail.com'
+    })
+
+    expect(createCustomerService.execute({
+      name: 'Fulano',
+      email: 'spfcarthur1@gmail.com'
+    })).rejects.toBeInstanceOf(AppError);
+
   })
 })
