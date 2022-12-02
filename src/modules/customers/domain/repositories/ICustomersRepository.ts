58337797945f8ca6eb4a03e008355bd3232e7684
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-12-02 01:07:40 -0300

    refactor: changing method signature

--- src/modules/customers/domain/repositories/ICustomersRepository.ts
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -6,7 +6,7 @@ export interface ICustomersRepository {
   save(customer: ICustomer): Promise<ICustomer>;
   findByName(name: string): Promise<ICustomer | undefined>;
   findByEmail(email: string): Promise<ICustomer | undefined>;
-  findAll(): Promise<ICustomer[]>;
+  findAll(): Promise<ICustomer[] | undefined>;
   remove(customer: ICustomer): Promise<void>;
   findOne(id: string): Promise<ICustomer | undefined>;
 }
