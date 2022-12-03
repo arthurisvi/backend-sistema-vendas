6e9621227fb2db3d5f2d5369f8e2936dceabcf6b
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-12-02 23:12:59 -0300

    refactor: desacoupling login service

--- src/modules/users/infra/http/controllers/AuthController.ts
+++ src/modules/users/infra/http/controllers/AuthController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { container } from 'tsyringe';
 import LoginService from '../../../services/LoginService';
 import { instanceToInstance } from 'class-transformer';
 export default class AuthController{
@@ -6,7 +7,7 @@ export default class AuthController{
   public async create(request: Request, response: Response): Promise<Response>{
     const { email, password } = request.body;
 
-    const loginService = new LoginService();
+    const loginService = container.resolve(LoginService);
 
     const session = await loginService.execute({ email, password })
 
--- src/modules/users/services/LoginService.ts
+++ src/modules/users/services/LoginService.ts
@@ -1,11 +1,10 @@
-import { getCustomRepository } from "typeorm"
 import { Secret, sign } from "jsonwebtoken"
-import UserRepository from "../infra/typeorm/repositories/UsersRepository";
 import User from "@modules/users/infra/typeorm/entities/User";
 import AppError from "@shared/errors/AppError";
 import authConfig from "@config/auth";
 import { inject, injectable } from "tsyringe";
 import { IHashProvider } from "../providers/HashProvider/models/IHashProvider";
+import { IUsersRepository } from "../domain/repositories/IUsersRepository";
 
 interface IRequest {
   email: string;
@@ -21,14 +20,14 @@ interface IResponse {
 export default class LoginService {
 
   constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
     @inject('HashProvider')
     private hashProvider: IHashProvider
   ){}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
-    const userRepository = getCustomRepository(UserRepository)
-
-    const user = await userRepository.findByEmail(email)
+    const user = await this.usersRepository.findByEmail(email)
 
     if (!user) {
       throw new AppError('Usuário e/ou senha incorretos.', 401)
